c761184124ab82e2ffbfab95de30726db867eac8
Pieter Jan <pieterjaninfo@gmail.com>
2017-02-09 16:43:10 +0100

    Python practicum graphs ex2finished

---
+++ src/Practicum/tutorial3/complement.py
@@ -0,0 +1,18 @@
+from src.Practicum.tutorial3.graph import *
+from src.Practicum.tutorial3.exercise2 import *
+
+
+def complement(file_in, file_out):
+    g = open_graph(file_in)
+    g_vertices_size = len(g.vertices)
+    graph = Graph(False, g_vertices_size)
+
+    for i in range(0, g_vertices_size):
+        for j in range(i + 1, g_vertices_size):
+            if not g.is_adjacent(g.vertices[i], g.vertices[j]):
+                graph += Edge(graph.vertices[i], graph.vertices[j])
+    write_graph(graph, file_out)
+
+
+if __name__ == '__main__':
+    complement('resources/graphs/short_graph.gr', 'resources/made_graphs/complement_graph.gr')
---
+++ src/Practicum/tutorial3/exercise2.py
@@ -0,0 +1,27 @@
+from src.Practicum.tutorial3.graph_io import load_graph, save_graph
+from src.Practicum.tutorial3.exercise1 import *
+
+
+def open_graph(filename):
+    with open('../../../' + filename) as f:
+        graph = load_graph(f)
+    return graph
+
+
+def write_graph(graph, filename):
+    with open('../../../' + filename, 'w') as f:
+        save_graph(graph, f)
+
+
+if __name__ == '__main__':
+    g = open_graph('resources/graphs/examplegraph.gr')
+
+    v = Vertex(g)
+    g += v
+    g += Edge(g.vertices[0], v)
+    write_graph(g, 'resources/made_graphs/new_examplegraph.gr')
+    print(g)
+
+    h = open_graph('resources/made_graphs/new_examplegraph.gr')
+    print(h)
+
